24ced9aa42d307ebb2a876bd1f95f1de39e7e520
Brenda <73964041+Brenda403@users.noreply.github.com>
2023-04-19 15:42:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let message = document.querySelector("#message");
+
+console.log("hello world");
+
+function addMovie(event) {
+  event.preventDefault();
+  let inputField = document.querySelector("input");
+  let movie = document.createElement("li");
+  let movieTitle = document.createElement("span");
+  movieTitle.addEventListener("click", crossOffMovie);
+  movieTitle.textContent = inputField.value;
+  movie.appendChild(movieTitle);
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", deleteMovie);
+  movie.appendChild(deleteBtn);
+  document.querySelector("ul").appendChild(movie);
+  inputField.value = "";
+}
+
+document.querySelector("form").addEventListener("submit", addMovie);
+function deleteMovie(event) {
+  event.target.parentNode.remove();
+  message.textContent = "Movie deleted!";
+}
+
+function crossOffMovie(event) {
+  event.target.classList.toggle("checked");
+  if (event.target.classList.contains("checked")) {
+    message.textContent = "Movie watched!";
+  } else {
+    message.textContent = "Movie added back!";
+  }
+}
